3311751db2054fc0480a02f1cf442205c6677d25
Jumpei Matsuda <jmatsu.drm@gmail.com>
2020-04-23 01:55:48 +0900

    Support Glob pattern

--- plugin/src/main/kotlin/io/github/jmatsu/license/internal/ArtifactIgnoreParser.kt
+++ plugin/src/main/kotlin/io/github/jmatsu/license/internal/ArtifactIgnoreParser.kt
@@ -1,6 +1,7 @@
 package io.github.jmatsu.license.internal
 
 import java.io.File
+import java.nio.file.FileSystems
 
 typealias IgnorePredicate = (group: String, name: String) -> Boolean
 
@@ -23,23 +24,41 @@ class ArtifactIgnoreParser(
 
         return when (format) {
             Format.Regex -> buildRegexPredicate(ignoreFile.readLines())
-            Format.Glob -> TODO()
+            Format.Glob -> buildGlobPredicate(ignoreFile.readLines())
         }
     }
 
     fun buildRegexPredicate(lines: List<String>): IgnorePredicate {
-        val pattern = lines.mapNotNull {
+        val pattern = lines.rejectComments().joinToString("|") { "^$it$" } // enclose by beginning/end match
+
+        val regex = Regex(pattern, RegexOption.IGNORE_CASE)
+
+        return { group, name ->
+            regex.matches("$group:$name")
+        }
+    }
+
+    fun buildGlobPredicate(lines: List<String>): IgnorePredicate {
+        val matchers = lines.map { glob ->
+            FileSystems.getDefault().getPathMatcher("glob:${glob.replace(":", "/")}")
+        }
+
+        return { group, name ->
+            val path = FileSystems.getDefault().getPath(group, name)
+
+            matchers.any {
+                it.matches(path)
+            }
+        }
+    }
+
+    private fun List<String>.rejectComments(): List<String> {
+        return mapNotNull {
             // Whitespaces are not allowed because these values based on module groups and/or names.
             it.split(Regex("\\s")).firstOrNull()
         }.filterNot {
             // Reject comments but this should be done after trimming whitespaces to support several expected comment usecases like `abc:xyz # comments`
             "#" in it || it.isBlank()
-        }.joinToString("|") { "^$it$" } // enclose by beginning/end match
-
-        val regex = Regex(pattern)
-
-        return { group, name ->
-            regex.matches("$group:$name")
         }
     }
 }
--- plugin/src/test/kotlin/io/github/jmatsu/license/internal/ArtifactIgnoreParserTest.kt
+++ plugin/src/test/kotlin/io/github/jmatsu/license/internal/ArtifactIgnoreParserTest.kt
@@ -100,6 +100,21 @@ class ArtifactIgnoreParserTest {
         }
 
         @Test
+        fun `ignore artifacts that do not have the prefix`() {
+            ignoreFile.writeText("""
+            prefix-.*
+        """.trimIndent())
+
+            val predicate = parser.buildPredicate(ArtifactIgnoreParser.Format.Regex)
+
+            assertNotNull(predicate)
+            assertTrue(predicate("prefix-", "artifact-any"))
+            assertTrue(predicate("prefix-", "artifact-"))
+            assertFalse(predicate("prefix", "artifact"))
+            assertFalse(predicate("any", "artifact-any"))
+        }
+
+        @Test
         fun `work even if chomped file`() {
             ignoreFile.writeText("""
             specific:artifact
@@ -120,6 +135,7 @@ class ArtifactIgnoreParserTest {
             ignore_group:.*
             several:artifact-.*
             several-group-[^:]+:.+
+            prefix-.*
         """.trimIndent())
 
             val predicate = parser.buildPredicate(ArtifactIgnoreParser.Format.Regex)
@@ -143,6 +159,147 @@ class ArtifactIgnoreParserTest {
             assertTrue(predicate("several-group-other", "group"))
             assertTrue(predicate("several-group-other", "any"))
             assertFalse(predicate("several-group", "any"))
+
+            assertTrue(predicate("prefix-", "artifact-any"))
+            assertTrue(predicate("prefix-", "artifact-"))
+            assertFalse(predicate("prefix", "artifact"))
+        }
+    }
+
+    @Nested
+    @DisplayName("GlobIgnore")
+    inner class GlobIgnore {
+        @Test
+        fun `predicate should be null unless ignoreFile exists`() {
+            ignoreFile.delete()
+
+            val predicate = parser.buildPredicate(ArtifactIgnoreParser.Format.Glob)
+
+            assertNull(predicate)
+        }
+
+        @Test
+        fun `ignore specific artifact`() {
+            ignoreFile.writeText("""
+            specific:artifact
+        """.trimIndent())
+
+            val predicate = parser.buildPredicate(ArtifactIgnoreParser.Format.Glob)
+
+            assertNotNull(predicate)
+            assertTrue(predicate("specific", "artifact"))
+            assertFalse(predicate("specific", "artifac2"))
+            assertFalse(predicate("specific2", "artifact"))
+        }
+
+        @Test
+        fun `ignore group`() {
+            ignoreFile.writeText("""
+            ignore_group:*
+        """.trimIndent())
+
+            val predicate = parser.buildPredicate(ArtifactIgnoreParser.Format.Glob)
+
+            assertNotNull(predicate)
+            assertTrue(predicate("ignore_group", "one"))
+            assertTrue(predicate("ignore_group", "two"))
+            assertFalse(predicate("group", "ignore_group"))
+        }
+
+        @Test
+        fun `ignore several artifacts`() {
+            ignoreFile.writeText("""
+            several:artifact-*
+        """.trimIndent())
+
+            val predicate = parser.buildPredicate(ArtifactIgnoreParser.Format.Glob)
+
+            assertNotNull(predicate)
+            assertTrue(predicate("several", "artifact-any"))
+            assertTrue(predicate("several", "artifact-"))
+            assertFalse(predicate("several", "artifact"))
+            assertFalse(predicate("any", "artifact-any"))
+        }
+
+        @Test
+        fun `ignore several groups`() {
+            ignoreFile.writeText("""
+            several-group-*:*
+        """.trimIndent())
+
+            val predicate = parser.buildPredicate(ArtifactIgnoreParser.Format.Glob)
+
+            assertNotNull(predicate)
+            assertTrue(predicate("several-group-any", "group"))
+            assertTrue(predicate("several-group-other", "group"))
+            assertTrue(predicate("several-group-other", "any"))
+            assertFalse(predicate("several-group", "any"))
+        }
+
+        @Test
+        fun `ignore artifacts that do not have the prefix`() {
+            ignoreFile.writeText("""
+            prefix-**
+        """.trimIndent())
+
+            val predicate = parser.buildPredicate(ArtifactIgnoreParser.Format.Glob)
+
+            assertNotNull(predicate)
+            assertTrue(predicate("prefix-", "artifact-any"))
+            assertTrue(predicate("prefix-", "artifact-"))
+            assertFalse(predicate("prefix", "artifact"))
+            assertFalse(predicate("any", "artifact-any"))
+        }
+
+        @Test
+        fun `work even if chomped file`() {
+            ignoreFile.writeText("""
+            specific:artifact
+        """.trimIndent().trimEnd { it.isWhitespace() || it == '\n' || it == '\r' })
+
+            val predicate = parser.buildPredicate(ArtifactIgnoreParser.Format.Glob)
+
+            assertNotNull(predicate)
+            assertTrue(predicate("specific", "artifact"))
+            assertFalse(predicate("specific", "artifact2"))
+            assertFalse(predicate("specific2", "artifact"))
+        }
+
+        @Test
+        fun `ignore all examples at once`() {
+            ignoreFile.writeText("""
+            specific:artifact
+            ignore_group:*
+            several:artifact-*
+            several-group-*:*
+            prefix-**
+        """.trimIndent())
+
+            val predicate = parser.buildPredicate(ArtifactIgnoreParser.Format.Glob)
+
+            assertNotNull(predicate)
+
+            assertTrue(predicate("specific", "artifact"))
+            assertFalse(predicate("specific", "artifact2"))
+            assertFalse(predicate("specific2", "artifact"))
+
+            assertTrue(predicate("ignore_group", "one"))
+            assertTrue(predicate("ignore_group", "two"))
+            assertFalse(predicate("group", "ignore_group"))
+
+            assertTrue(predicate("several", "artifact-any"))
+            assertTrue(predicate("several", "artifact-"))
+            assertFalse(predicate("several", "artifact"))
+            assertFalse(predicate("any", "artifact-any"))
+
+            assertTrue(predicate("several-group-any", "group"))
+            assertTrue(predicate("several-group-other", "group"))
+            assertTrue(predicate("several-group-other", "any"))
+            assertFalse(predicate("several-group", "any"))
+
+            assertTrue(predicate("prefix-", "artifact-any"))
+            assertTrue(predicate("prefix-", "artifact-"))
+            assertFalse(predicate("prefix", "artifact"))
         }
     }
 }
